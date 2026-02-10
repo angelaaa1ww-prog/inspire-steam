ff4353592d815f60a6757d44939260df291b0778
angelaaa1ww-prog <angelaaa1ww@gmail.com>
2026-02-10 21:19:55 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,9 @@
+university="UON"
+Grade="A-"
+Favourite_course="Software engineering"
+Cluster_points=43.2
+print(f"My favourite University is {university}")
+print(f"I got a mean grade of {Grade}")
+print(f"And my favourite course is {Favourite_course}")
+print(f"My cluster points are {Cluster_points} points")
+
