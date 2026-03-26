a9ed3811c0a8d1325931b683afbb7d403eb40ed5
angelaaa1ww-prog <angelaaa1ww@gmail.com>
2026-03-26 13:59:00 +0300

    web

--- Javascript/cpy.js
+++
@@ -1,4 +0,0 @@
-age = 21
-if(age>= 18){
-    console.log("You are allowed to drive");
-}else{console.log("You are not allowed to drive");}
--- Javascript/lesson2.js
+++ Javascript/lesson2.js
@@ -1,14 +1,18 @@
-//This is a single line comment
+// This is a single line comment
 
- is a multiline comment
+/* This is a multiline comment */
 
+let age = 28;
 
-age = 28;
-if(age>= 18){
+if (age >= 18) {
     console.log("You are allowed to drive");
-}else{
-    console.log("You are not allowed to drive");}
+} else {
+    console.log("You are not allowed to drive");
+}
 
-while(age<=20){console.log(age)}
+while (age <= 20) {
+    console.log(age);
+    age++;
+}
 
 
