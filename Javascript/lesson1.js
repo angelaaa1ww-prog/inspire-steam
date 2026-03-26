8760819a20a0357a77c04db85b36709f2f7a3a3e
angelaaa1ww-prog <angelaaa1ww@gmail.com>
2026-03-26 10:28:39 +0300

    web

---
+++ Javascript/lesson1.js
@@ -0,0 +1,27 @@
+
+
+Name = "Amani Angela"
+grade= "B"
+age= 18
+
+weight= 55.6
+
+fav_color= "Blue"
+
+
+f_number= 45
+s_number= 15
+sum_numbers= f_number + s_number
+diff_numbers= f_number - s_number
+quotient_numbers= f_number / s_number
+prod_numbers= f_number * s_number
+
+console.log("sum of the numbers =" + sum_numbers)
+console.log("difference of the numbers =" + diff_numbers)
+console.log("quotient of the numbers =" + quotient_numbers)
+console.log("product of the numbers =" + prod_numbers)
+console.log("Hello! from" + Name)
+console.log("I am" + age + "years old")
+
+
+
---
+++ Javascript/lesson2.js
@@ -0,0 +1,14 @@
+//This is a single line comment
+
+ is a multiline comment
+
+
+age = 28;
+if(age>= 18){
+    console.log("You are allowed to drive");
+}else{
+    console.log("You are not allowed to drive");}
+
+while(age<=20){console.log(age)}
+
+
